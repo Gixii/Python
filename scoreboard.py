b262f59d98e1a58e00c8d0641caf2f22fd7e929e
Pranjal Malik <128349379+Gixii@users.noreply.github.com>
2024-01-20 03:15:15 +0000

    Update scoreboard.py
    Add High Score

--- scoreboard.py
+++ scoreboard.py
@@ -14,6 +14,8 @@ class Scoreboard(Turtle):
         # Initializing the Turtle properties
         super().__init__()  # Calls the constructor of the Turtle class
         self.score = 0  # Setting the initial score to 0
+        with open("data.txt") as file: # Opening data.txt containg our High Score intialed to 0
+            self.high_score = int(file.read())
         self.color("white")  # Setting text color to white
         self.penup()  # Not drawing while moving
         self.goto(0, 270)  # Positioning the scoreboard at (0, 270)
@@ -24,6 +26,7 @@ class Scoreboard(Turtle):
         """
         Updates the scoreboard with the current score.
         """
+        self.clear()  # Clearing the previous score display
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
@@ -31,12 +34,17 @@ class Scoreboard(Turtle):
         Increases the score by 1 and updates the scoreboard display.
         """
         self.score += 1  # Incrementing the score by 1
-        self.clear()  # Clearing the previous score display
         self.update_scoreboard()  # Updating the scoreboard with the new score
 
-    def game_over(self):
-        """
-        Displays "GAME OVER" at the center of the screen.
-        """
-        self.goto(0, 0)  # Moving to the center of the screen
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)  # Displaying "GAME OVER"
+    def resett(self):
+    # Check if the current score is higher than the high score
+    if self.score > self.high_score:
+        # If yes, update the high score with the current score
+        self.high_score = self.score
+        # Open the file in write mode and write the new high score to "data.txt"
+        with open("data.txt", mode="w") as file:
+            file.write(f"{self.high_score}")
+    # Reset the current score to 0
+    self.score = 0
+    self.update_scoreboard()
+
